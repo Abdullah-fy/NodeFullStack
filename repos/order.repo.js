9ff5a01a034283801b0ab02dd31fafb31bb7c6ee
Mariam Mounir <mounirsalama130@yahoo.com>
2025-02-05 12:05:17 +0200

    order repo

--- repos/order.repo.js
+++ repos/order.repo.js
@@ -1,27 +1,46 @@
 const Order = require('../models/order.model');
 
-class orderRebo {
+class orderRepo {
     //1-find all ==> for admin
-    static async findAllOrder(){
+    static async findAllOrder({ page = 1, limit = 10, sortBy = "createdAt", sortedOrder = "desc", filter = {} } = {}) {
         try {
-            const orders= await Order.find();
-            return orders;       
+
+            if (isNaN(page) || isNaN(limit) || page < 1 || limit < 1 || limit > 100) {
+                throw new Error('Invalid pagination parameters');
+            }
+
+            const skip = (page - 1) * limit;
+
+            const orders = await Order.find(filter)
+                .sort({ [sortBy]: sortedOrder === 'desc' ? -1 : 1 })
+                .skip(skip)
+                .limit(limit);
+
+            const totalorders = await Order.countDocuments(filter)
+
+            return {
+                orders,
+                currentPage: page,
+                totalPages: Math.ceil(totalorders / limit),
+                totalorders
+            };
         } catch (error) {
-            throw new Error('Error Acessing orders')
+            throw new Error('Error acessing orders')
         }
 
     }
-    //1-find order by customer id
+
+    //2-find orders by customer id
     static async findOrderByCustomerId(customerId) {
         try {
-            const order = await Order.findOne({ customerId });
-            return order
+            const orders = await Order.find({ customerId });
+            return orders
         } catch (error) {
             throw new Error('Error finding order, Please try again later. ');
         }
     }
 
-    //2-Create new Order
+    //3-Create new Order
     static async createOrder(orderData) {
         try {
             const order = new Order(orderData);
@@ -30,4 +49,53 @@ class orderRebo {
             throw new Error('Error placing order. Please try again later')
         }
     }
+
+    //4-Update Order
+    static async updateOrder(_id, updateData) {
+        try {
+            const updateOrder = await Order.findByIdAndUpdate(
+                _id,
+                { $set: updateData },
+                { new: true },
+            );
+            if (!updateOrder) {
+                throw new Error("Order not found");
+            }
+            return updateOrder
+        } catch (error) {
+            throw new Error("order not found")
+        }
+    }
+
+    //5-get order by sellerId ==> for seller
+    static async findOrderBySellerId(sellerId)
+    {
+        try{
+            const orders =await Order.find({'items.sellerId':sellerId});
+            return orders
+        }catch(error){
+            throw new Error('Error finding orders by sellerId');
+        }
+    }
+
+    //6-update item statues
+    static async updateItemStatus(orderId,productId,newStatus){
+        try{
+            const order =await Order.findById(orderId);
+            if(!order){
+                throw new Error('Order not Found');
+            }
+            const item = order.items.find(i => i.productId === productId);
+            if(!item){
+                throw new Error('Item not found')
+            }
+            item.itemStatus=newStatus;
+            await order.save();
+
+            return order
+        }catch(error){
+            throw new Error(`Error updating item status: ${error.message}`);
+        }
+    }
+
 }
