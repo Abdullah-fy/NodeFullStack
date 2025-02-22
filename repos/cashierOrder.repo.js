3d5cdeb38cbe54cac9ab46eae2276f0ffc592478
Mariam Mounir <mounirsalama130@yahoo.com>
2025-02-22 23:25:07 +0200

    Cashier

--- repos/cashierOrder.repo.js
+++ repos/cashierOrder.repo.js
@@ -1,4 +1,7 @@
 const Order=require('../models/CashierOrder.model');
+const staff=require('../models/staff')
+const inventory=require('../models/Inventory')
+const Cart=require('../models/cart.model')
 
 class CashierOrderRebo{
 //1-find all ==> for admin 
@@ -50,6 +53,36 @@ static async createOrder(orderData) {
         return await order.save();
     } catch (error) {
         throw new Error(`Error placing order. Please try again later ${error.message}`)
+    }
+}
+
+//4-cashier detail
+static async cashierdetail(CashierId) {
+    try {
+        const cashier = await staff.findById(CashierId)
+        return cashier
+    } catch (error) {
+        throw new Error (`Error finding cashier ${error.message}`)
+    }
+}
+
+//5-get product in inventory
+static async inventory(inventoryId)
+{
+    try {
+        return await inventory.findById(inventoryId)
+    } catch (error) {
+        throw new Error (`Error reashing inventory ${error.message}`)
+    }
+}
+
+//6-Create new cart
+static async createCart(cartData) {
+    try {
+        const cart = new Cart(cartData);
+        return await cart.save();
+    } catch (error) {
+        throw new Error('Error creating cart. Please try again later.');
     }
 }
 }
