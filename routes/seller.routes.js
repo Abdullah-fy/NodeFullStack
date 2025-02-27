09c6c5847ab607b60831b66ef239871a10e628f5
Mariam Mounir <mounirsalama130@yahoo.com>
2025-02-27 02:15:56 +0200

    last

--- routes/seller.routes.js
+++ routes/seller.routes.js
@@ -8,11 +8,11 @@ router.get('/products',authConrroller.protect, authConrroller.restrictTo('seller
 router.put('/products/:id',authConrroller.protect, authConrroller.restrictTo('seller'),SellerProductsController.updateAProduct);
 router.post('/products',authConrroller.protect, authConrroller.restrictTo('seller'),SellerProductsController.AddProduct);
 router.delete('/products/:id',authConrroller.protect, authConrroller.restrictTo('seller'),SellerProductsController.DeleteProduct);
-router.get('/products/:id',authConrroller.protect, authConrroller.restrictTo('seller'),SellerProductsController.GetProductByID); //check seller id 
+router.get('/products/:id',SellerProductsController.GetProductByID); //check seller id 
 //router.get('/products/seller/:sellerId', SellerProductsController.getProductsBySeller);
 //router.patch('/products/:id/stock', SellerProductsController.updateStock);
-router.get('/filteredProducts', authConrroller.protect, authConrroller.restrictTo('seller'),SellerProductsController.getFilteredProducts);
-router.get("/onlineProducts",authConrroller.protect, authConrroller.restrictTo('seller'), SellerProductsController.getOnlineProducts);
+router.get('/filteredProducts',SellerProductsController.getFilteredProducts);
+router.get("/onlineProducts", SellerProductsController.getOnlineProducts);
 
 
 module.exports=router;
