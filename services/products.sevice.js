46161385272979de9eb2a7dec522025b9fadac67
Esraa Abas <esraa.abbas19@gmail.com>
2025-03-03 16:12:17 +0200

    conflicts resolved

--- services/products.sevice.js
+++ services/products.sevice.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 const {getProducts, AddProduct, DeleteProduct, UpdateProduct, getById,getProductsBySeller_, getFilteredProducts,getUnActiveProducts, getOnlineProducts}= require ('../repos/products.repos');
-
-=======
-const { json } = require('express');
-const {getProducts, AddProduct, DeleteProduct, UpdateProduct, getById, getFilteredProducts,getUnActiveProducts, getOnlineProducts}= require ('../repos/products.repos');
->>>>>>> 2fc68262c654ac18be09835c47405d55eaa238c7
 const ProductRepo = require('../repos/products.repos');
 const {upload} =require ('./media.service');
 const mongoose=require ('mongoose');
@@ -19,97 +13,6 @@ const getOnlineProductsService = async () => {
     return products;
   };
 
-<<<<<<< HEAD
-=======
-const GetUproducts=async ()=>{
-    console.log("Inside getproducts service");
-    return getProducts();
-};
-
-const GetProductById = async (id) => {
-    const product = await getById(id);
-    if (!product) {
-      throw new Error('Product not found');
-    }
-    return product;
-};
-
-//get prod by sel id 
-const GetProductsBySeller = async (sellerId) => {
-    console.log("Inside GetProductsBySeller service for sellerId:", sellerId);
-    return getProductsBySeller(sellerId);
-};
-
-
-const CreateProduct = async (req) => {
-    console.log(`Inside createproduct service`);
-    console.log(req.body._id);
-    console.log(req.files?.images);
-    if (!req.files?.images) {
-        console.log('error');
-        throw new Error("Please upload at least one image");
-    }
-    else
-        console.log('uploaded');
-
-    const imagesArray = Array.isArray(req.files.images) 
-    ? req.files.images 
-    : [req.files.images];
-
-    const imageUrls = await upload(imagesArray);
-
-    const productId = new mongoose.Types.ObjectId();
-
-
-    const stockData = {
-        productId: productId,
-        supplierId: req.body.supplierId,
-        status: 'approved'
-    };
-
-    const newStock = new Stock(stockData);
-    await newStock.save();
-
-    const productData = {
-        _id: req.body._id,
-        name: req.body.name,
-        description: req.body.description,
-        price: Number(req.body.price),
-        category: req.body.category,
-        images: imageUrls,
-        stockQuantity: Number(req.body.stockQuantity),
-        sellerinfo:JSON.parse(req.body.sellerInfo),
-        // sellerinfo: {
-        //     id: req.body.sellerinfoid, 
-        //     name: req.body.sellerinfoname
-        // }
-    };
-
-    console.log(productData);
-
-    console.log('from service :'+productData);
-
-    return AddProduct(productData);
-};
-
-const deleteProduct = async (id) => {
-    console.log("Inside delete product service");
-    const deletedproduct= DeleteProduct(id);
-    if(!deletedproduct){
-        throw new Error('Product not found');
-    }
-    return deletedproduct;
-};
-
-const updateProduct=async(id, data)=>{
-    console.log("Inside delete product service");
-    const updatedproduct= UpdateProduct(id,data);
-    if(!updatedproduct)
-        throw new Error('product not found');
-    return updatedproduct;
-};
-
->>>>>>> 2fc68262c654ac18be09835c47405d55eaa238c7
 //fetch filtered products
 const getFilteredProductsServices = async (filter) => {
     try {
@@ -256,11 +159,8 @@ module.exports={
     updateProduct,
     GetProductById,
     GetProductsBySeller,
-<<<<<<< HEAD
 
     //fatma
-=======
->>>>>>> 2fc68262c654ac18be09835c47405d55eaa238c7
     getFilteredProductsServices,
     getAllProductUnactive,
     SoftDeleteProduct,
